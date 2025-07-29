c75f70b5b8b0e6cbd7dbf2bf19b618ab3379c018
SONBORA <alclsloo@naver.com>
2025-07-29 22:42:47 +0900

    user테이블 email컬럼에 유니크제약사항 추가

--- db/schema.ts
+++ db/schema.ts
@@ -5,7 +5,7 @@ import { relations } from 'drizzle-orm';
 export const user = pgTable('user', {
     id: uuid('id').defaultRandom().primaryKey(),
     name: text('name').notNull(),
-    email: text('email').notNull(),
+    email: text('email').notNull().unique(),
     password: text('password').notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(), //생성날짜
     updatedAt: timestamp('updated_at').defaultNow().notNull(), //수정날짜
